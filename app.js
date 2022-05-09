80ae8dafec73a2c7dcf4adf5c62d75af49433d9b
Ade Yahya <adeyahyaprasetyo@gmail.com>
2022-05-09 13:57:48 +0700

    mvp

---
+++ app.js
@@ -0,0 +1,52 @@
+const yaml = require("js-yaml");
+const { exec } = require("child_process");
+const minimatch = require("minimatch");
+const fs = require("fs/promises");
+const ora = require("ora");
+
+const pattern = "**/hasura/**/*.yaml";
+
+const YAML_CONFIG = {
+	noArrayIndent: true,
+	quotingType: '"',
+	lineWidth: -1,
+};
+
+exec("git diff HEAD --name-only --diff-filter=ACM", async (error, stdout) => {
+	const rawFiles = stdout?.split(/\n/g);
+	const filteredFiles = rawFiles.filter(item => minimatch(item, pattern));
+	const filesIter = filteredFiles[Symbol.iterator]();
+	let isError = false;
+	const walker = async () => {
+		const file = filesIter.next();
+		if (!file.value) {
+			if (isError) {
+				console.error("Please re-commit your changes.");
+				process.exit(1);
+			} else {
+				process.exit(0);
+			}
+		}
+
+		const spinner = ora(`Checking ${file.value}`).start();
+
+		const rawStr = await fs.readFile(file.value, "utf8");
+		const jsonDef = yaml.load(rawStr);
+		const formattedYaml = yaml.dump(jsonDef, YAML_CONFIG);
+		if (formattedYaml !== rawStr) {
+			isError = true;
+			spinner.stopAndPersist({
+				symbol: "❌",
+				text: `Invalid ${file.value}`,
+			});
+			await fs.writeFile(file.value, formattedYaml);
+		} else {
+			spinner.stopAndPersist({
+				symbol: "✅",
+				text: `Valid ${file.value}`,
+			});
+		}
+		await walker();
+	}
+	await walker()
+});
